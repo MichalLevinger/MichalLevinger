0901579d88bd393ce2052bd113e6375abab4c1ae
MichalLevinger <michal45699@gmail.com>
2021-10-04 14:19:57 +0300

    with data

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Address(models.Model):
+    city = models.IntegerField()
+    country = models.IntegerField()
+    street = models.TextField(max_length=200)
+
+class Permission(models.Model):
+    type = models.CharField(max_length=30)
+    action = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='images')
+
+
+
+
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
 
+from .models import Permission
 
 def index(request):
-    return render(request, 'polls/index.html')
+    # permissions = [
+    #     {'type': 'manager',
+    #      'action': 'reset'
+    #      },
+    #     {
+    #         'type': 'junior',
+    #         'action': 'save'
+    #     },
+    #     {
+    #         'type': 'senior',
+    #         'action': 'cancel'
+    #     }
+    # ]
+    permissions = Permission.objects.all()
+    location_files = ['page 1', 'page 2']
+    return render(request, 'polls/index.html', {
+        'show': True,
+        'disable': False,
+        'permissions': permissions,
+        'location_files': location_files
+    })
+
+
+def action_page(request, action):
+    selected_permission = Permission.objects.get(action=action)
+    return render(request, 'polls/action-page.html', {'action': selected_permission.action})
+
